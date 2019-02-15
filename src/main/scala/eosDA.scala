00a241b14d3aa2aeabc7d055c97d74d27216d813
Vladimir <vldmr.weider@gmail.com>
2019-02-15 16:45:33 +0300

    global view

--- src/main/scala/eosDA.scala
+++ src/main/scala/eosDA.scala
@@ -1,6 +1,8 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.log4j.{Level, Logger}
 
+case class Account(acc_name:String, rate:String)
+
 object eosDA {
   def main(args: Array[String]) {
 
@@ -8,19 +10,30 @@ object eosDA {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
 
-    val logFile = "/usr/lib/spark/README.md"
+   // val logFile = "/usr/lib/spark/README.md"
     val spark = SparkSession
       .builder
       .appName("Simple Application")
       .config("spark.master", "local")
       .getOrCreate()
 
+    import spark.implicits._
 
 
-    val logData = spark.read.textFile(logFile).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
+
+    val df = spark.read.json("/home/vldmr/work/m2h/14022019.json").as[Account]
+
+    df.show(20)
+
+
+    //df.createOrReplaceTempView("account")
+    //spark.sql("select * from account").show(20)
+
+//    val logData = spark.read.textFile(logFile).cache()
+//
+//    val numAs = logData.filter(line => line.contains("a")).count()
+//    val numBs = logData.filter(line => line.contains("b")).count()
+//    println(s"Lines with a: $numAs, Lines with b: $numBs")
     spark.stop()
   }
 }
